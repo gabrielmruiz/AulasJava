6cf6861e833348edb1be2de6c7a765f6ff684f75
Thunder8598 <51998039+Thunder8598@users.noreply.github.com>
2021-02-03 21:24:22 -0300

    Update RodaOrc.java

--- RodaOrc.java
+++ RodaOrc.java
@@ -1,11 +1,15 @@
 package lista2;
 
+import java.math.BigDecimal;
+
 public class RodaOrc {
 
 	public static void main(String[] args) {
 		
-		OrcFinan of1 = new OrcFinan("Antonio", BigDecimal 475.87);
+		OrcFinan of1 = new OrcFinan("Antonio", new BigDecimal(475.87));
 		
 		of1.deposita10();
+		
+		System.out.println(of1.getSaldo());
 	}
 }
