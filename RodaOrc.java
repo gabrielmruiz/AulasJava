19b8d836bf64e81033587bed52e0b69366ae96c3
Gabriel Ruiz <70815190+gabrielmruiz@users.noreply.github.com>
2021-02-22 20:47:15 -0300

    Update RodaOrc.java

--- RodaOrc.java
+++ RodaOrc.java
@@ -1,11 +1,18 @@
-package lista2;
+import java.math.BigDecimal;
 
 public class RodaOrc {
 
 	public static void main(String[] args) {
 		
-		OrcFinan of1 = new OrcFinan("Antonio", BigDecimal 475.87);
+		OrcFinan of1 = new OrcFinan("Antonio", new BigDecimal (475.87));
 		
 		of1.deposita10();
+		of1.deposita10();
+		of1.saca10();
+		of1.saca10();
+		of1.saca10();
+		
+		System.out.println(of1.getSaldo());
+		System.out.println("A transação foi realizada em " + of1.getDiahora() );
 	}
 }
